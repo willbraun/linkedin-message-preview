0d156025f3b0dc22109341d90b70c4c8cddec886
willbraun <williamhbraun1@gmail.com>
2023-08-29 08:51:52 -0400

    added typescript

--- content-script.js
+++
@@ -1,8 +0,0 @@
-;(function() {
-  const shadowRoot = document.createElement('div').attachShadow({ mode: 'open' })
-	const bubble = document.createElement('div')
-	bubble.classList.add('message-preview-bubble')
-	bubble.innerHTML = ''
-	shadowRoot.appendChild(bubble)
-	document.body.appendChild(shadowRoot)
-})()
---
+++ src/content-script.ts
@@ -0,0 +1,15 @@
+;(function () {
+	console.log('hi')
+	const shadowRoot = document.createElement('div').attachShadow({ mode: 'open' })
+	const bubble = document.createElement('div')
+	bubble.classList.add('message-preview-bubble')
+	bubble.innerHTML = ''
+	shadowRoot.appendChild(bubble)
+	document.body.appendChild(shadowRoot)
+
+	const formatBubbleInnerHTML = (message: string) => (bubble.innerHTML = `<p>${message}</p>`)
+
+  // do this with mutation observer instead
+	const sidebarMessages = document.querySelectorAll('.msg-overlay-list-bubble__convo-item')
+	sidebarMessages.forEach(msg => msg.addEventListener('hover', () => formatBubbleInnerHTML('hi')))
+})()
