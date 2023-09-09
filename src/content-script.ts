65a886501906f4b6f8a75aa4867ae7a16e22adbb
willbraun <williamhbraun1@gmail.com>
2023-09-09 12:14:23 -0400

    fixed if bubble goes off top, and made polling smarter

--- src/content-script.ts
+++ src/content-script.ts
@@ -26,26 +26,33 @@
 				box.addEventListener('mouseenter', () => {
 					bubble.textContent = message
 					bubble.style.setProperty('max-width', `400px`)
+					bubble.style.setProperty('font-size', fontSize)
 
 					const bubbleRect1 = bubble.getBoundingClientRect()
 					const boxRect = box.getBoundingClientRect()
 
+					bubble.style.setProperty('right', `${boxRect.width + margin}px`)
+
 					if (bubbleRect1.height === maxHeight) {
 						bubble.style.setProperty('max-width', `${window.innerWidth - boxRect.width - margin * 2}px`)
-						bubble.style.setProperty('right', `${margin}px`)
 						bubble.style.setProperty('top', `${margin}px`)
 
+						// shrink font until bubble fits on screen
 						while (bubble.scrollHeight > bubbleRect1.height) {
 							const currentFontSize = Number(bubble.style.fontSize.split('px')[0])
 							bubble.style.setProperty('font-size', `${currentFontSize - 1}px`)
 						}
 					} else {
-						bubble.style.setProperty('font-size', fontSize)
-						bubble.style.setProperty('right', `${boxRect.width + margin}px`)
 						bubble.style.setProperty('top', `${boxRect.y + (boxRect.height - bubbleRect1.height) / 2}px`)
 
 						const bubbleRect2 = bubble.getBoundingClientRect()
 
+						// if bubble goes off top
+						if (bubbleRect2.y < margin) {
+							bubble.style.setProperty('top', `${margin}px`)
+						}
+
+						// if bubble goes off bottom
 						if (bubbleRect2.y + bubbleRect2.height > windowHeight - margin) {
 							bubble.style.setProperty('top', `${windowHeight - bubbleRect2.height - margin}px`)
 						}
@@ -65,19 +72,60 @@
 		mutationObserver.observe(sidebar, { childList: true })
 	}
 
-	let count = 0
+	let count1 = 0
+	let count2 = 0
+	let clickHandlerAdded = false
 
-	const intervalId = setInterval(() => {
-		count++
-		const sidebarHeader = document.querySelector('.msg-overlay-bubble-header')
-		if (!sidebarHeader && count === 15) {
-			clearInterval(intervalId)
+	const checkForMessages = (intervalId: number) => {
+		if (!clickHandlerAdded) {
+			const sidebarHeader = document.querySelector('.msg-overlay-bubble-header')
+			if (sidebarHeader) {
+				sidebarHeader.addEventListener('click', findSidebar, { capture: true })
+				clickHandlerAdded = true
+			}
 		}
 
 		const sidebar = document.querySelector('.msg-overlay-list-bubble__conversations-list')
 		if (sidebar) {
-			clearInterval(intervalId)
 			setupMouseOver(sidebar)
+			clearInterval(intervalId)
+		}
+	}
+
+	// check every 1s, then every 5s, then give up after 20 tries of each
+	const intervalId1 = setInterval(() => {
+		checkForMessages(intervalId1)
+
+		count1++
+		if (count1 === 20) {
+			const intervalId2 = setInterval(() => {
+				checkForMessages(intervalId2)
+
+				count2++
+				if (count2 === 20) {
+					clearInterval(intervalId2)
+				}
+			}, 5000)
+
+			clearInterval(intervalId1)
 		}
 	}, 1000)
+
+	const findSidebar = () => {
+		let sidebarCount = 0
+		const sidebarInterval = setInterval(() => {
+			const sidebar = document.querySelector('.msg-overlay-list-bubble__conversations-list')
+			if (sidebar) {
+				setupMouseOver(sidebar)
+				clearInterval(sidebarInterval)
+			}
+
+			sidebarCount++
+
+			// messages tab is closed
+			if (sidebarCount === 10) {
+				clearInterval(sidebarInterval)
+			}
+		}, 500)
+	}
 })()
